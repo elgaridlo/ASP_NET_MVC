07d60dbbbf8cff26ad1680fcd9ef389afbff3e3b
Elga <elgaridlosinatriya@gmail.com>
2021-07-23 11:28:45 +0700

    ASP.NET MVC without API

---
+++ Vidly_auth/ViewModel/NewCustomerViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_auth.Models;
+
+namespace Vidly_auth.ViewModel
+{
+    public class NewCustomerViewModels
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ Vidly_auth/ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_auth.Models;
+
+namespace Vidly_auth.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
